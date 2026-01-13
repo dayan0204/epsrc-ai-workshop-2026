e01239f03bc5bfcad0686f7d4ff546828455d25e
Yueran Ma <45652227+dayan0204@users.noreply.github.com>
2026-01-13 14:52:58 +0000

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,8 @@
+// Highlight active nav link based on current path
+(function(){
+  const path = location.pathname.split("/").pop() || "index.html";
+  document.querySelectorAll(".nav a[data-page]").forEach(a => {
+    if(a.getAttribute("data-page") === path) a.classList.add("active");
+  });
+})();
 
